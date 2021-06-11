49fc7086e6d48680639b332ee45875691b202a2d
ScottishGuy95 <32342101+ScottishGuy95@users.noreply.github.com>
2021-06-12 00:05:59 +0100

    Altered the program to include a way to keep a capitalization of the original text for encoding or decoding.

--- MultiplicativeCipher.py
+++ MultiplicativeCipher.py
@@ -29,23 +29,24 @@ def multiplicative(key, text, direction):
     Used to return an encoded or decoded string of text using a multiplicative cipher
     :param key: (int) A value used to encode the key, must result in a 1-to-1 mapping.
     Requires the key to have no common divisor or be a multiple or the alphabet length.
-    :param text: (str) The text being alterted using the cipher
+    :param text: (str) The text being altered using the cipher
     :param direction: (str) "Encode" or "decode" depending on the users requirement
     :return: (str) The altered string for the user
     """
+    # The alphabet linked to an int value from 0 to 25
     alphabet = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9, 'K': 10, 'L': 11,
                 'M': 12, 'N': 13, 'O': 14, 'P': 15, 'Q': 16, 'R': 17, 'S': 18, 'T': 19, 'U': 20, 'V': 21, 'W': 22,
                 'X': 23, 'Y': 24, 'Z': 25}
     value = key
     result = ""
-    for x in range(len(text)):
-        if text[x].isalpha():
-            if direction == "decode":
-                value = pow(key, -1, len(alphabet))
-            letter = (alphabet.get(text[x].upper()) * value) % len(alphabet)
-            result += getKey(letter, alphabet)
-        else:
-            result += text[x]
+    for x in range(len(text)):                          # Loop through the given text, checking for letters
+        # Decode by setting the using the original key to calculate the multiplicative inverse of the key
+        # The use that value to return the ciphertext to the plaintext alternative
+        # Otherwise encode using the key value to create the ciphertext
+        if direction == "decode":
+            value = pow(key, -1, len(alphabet))
+        letter = (alphabet.get(text[x].upper()) * value) % len(alphabet)
+        result += getKey(letter, alphabet)          # Gets the value responding to the given letter in the alphabet
     return result
 
 
@@ -66,10 +67,22 @@ text = input("Text (The phrase/word): ")
 while True:
     key = input("Please enter a key value: ")
 
+    # Used to verify that the given input has no common divider or a multiple or the alphabet length (26)
     if math.gcd(26, int(key)) != 1:
         print("\tInvalid key given")
     else:
         break
 
-cipher_result = multiplicative(int(key), text, request)
-print("Output: " + cipher_result)
+result = ""
+# Pass each letter to be encoded/decoded to the cipher, to allow the original capitalisation to be kept or reset
+for ltr in text:
+    if ltr.isalpha():
+        result_ltr = multiplicative(int(key), ltr, request)
+        if ltr.isupper():
+            result += result_ltr.capitalize()
+        else:
+            result += result_ltr.lower()
+    else:
+        result += ltr
+
+print("Output: " + result)
